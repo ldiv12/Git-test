cf5a96fa51721e8bddb2704122509a2ca2609d82
amani bhb <amanibhb21@gmail.com>
2024-08-27 10:40:39 +0200

    Ajout/modification du fichier X

---
+++ Fuck.py
@@ -0,0 +1,8 @@
+def draw_fuck():
+    print("  FFFFFFF  U     U  CCCCCCC  K     K")
+    print("  F        U     U  C        K    K ")
+    print("  FFFFFFF  U     U  C        KKKKK  ")
+    print("  F        U     U  C        K    K ")
+    print("  F         UUUUU   CCCCCCC  K     K")
+
+draw_fuck()
